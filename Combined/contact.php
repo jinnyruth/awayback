cf46017cda47e29fbf32af0a6f27ad3ff716ff35
jinnyruth <jinnyruth@gmail.com>
2022-10-07 10:20:05 -0600

    favicon

--- Combined/contact.php
+++ Combined/contact.php
@@ -9,6 +9,8 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         
        <link href="css/styles.css" rel="stylesheet" />
+       <link rel="icon" type="image/x-icon" href="favicon.ico">
+       
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 	<script src="https://kit.fontawesome.com/e8ea6126e1.js" crossorigin="anonymous"></script>
